78cd435452377f1c0d3cabff949477ddba6101b7
Fabio Fortini <fabiofortini@MacBook-Pro-di-Fabio.local>
2017-09-05 22:10:30 +0200

    2min 500c + no switch

--- app/src/main/java/com/campanello/focaga/campanello/MainActivity.java
+++ app/src/main/java/com/campanello/focaga/campanello/MainActivity.java
@@ -7,17 +7,15 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
 import java.io.File;
 import java.io.FileOutputStream;
 
 public class MainActivity extends Activity {
-    Button campanello;
+    Button campanello, cambio;
     MediaPlayer mediaPlayer = new MediaPlayer();
     MediaPlayer mediaPlayer2 = new MediaPlayer();
     Boolean jumpWhenPlaying = true;
-    Switch cambio;
+    Boolean checked = false;
     Object suono = R.raw.bell;
     int count;
     long start = -1;
@@ -38,7 +36,7 @@ public class MainActivity extends Activity {
         mAdView.loadAd(adRequest);*/
 
         campanello = (Button) findViewById(R.id.campanello);
-        cambio = (Switch) findViewById(R.id.cambio);
+        cambio = (Button) findViewById(R.id.cambio);
         count = 0;
 
         fileSave = new File(this.getFilesDir(), "vittoria.txt");
@@ -49,74 +47,75 @@ public class MainActivity extends Activity {
         campanello.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            //only when the cameriere is unlocked
+            if (start == -1 && !fileSave.exists()) {
+                start = System.currentTimeMillis();
+            }
 
-                //only when the cameriere is unlocked
-                if (start == -1 && !fileSave.exists()) {
-                    start = System.currentTimeMillis();
-                }
+            //manage the memory problem for the media player object
+            if (jumpWhenPlaying) {
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), (int)suono);
+                mediaPlayer.start();
+                jumpWhenPlaying = !jumpWhenPlaying;
+                mediaPlayer2.release();
+                count ++;
+            } else {
+                mediaPlayer2 = MediaPlayer.create(getApplicationContext(), (int)suono);
+                mediaPlayer2.start();
+                jumpWhenPlaying = !jumpWhenPlaying;
+                mediaPlayer.release();
+                count ++;
+            }
 
-                //manage the memory problem for the media player object
-                if (jumpWhenPlaying) {
-                    mediaPlayer = MediaPlayer.create(getApplicationContext(), (int)suono);
-                    mediaPlayer.start();
-                    jumpWhenPlaying = !jumpWhenPlaying;
-                    mediaPlayer2.release();
-                    count ++;
-                } else {
-                    mediaPlayer2 = MediaPlayer.create(getApplicationContext(), (int)suono);
-                    mediaPlayer2.start();
-                    jumpWhenPlaying = !jumpWhenPlaying;
-                    mediaPlayer.release();
-                    count ++;
-                }
+            //difference between the time of the first click and the current time
+            runTime = System.currentTimeMillis() - start;
+            //if you already won, start wibb be always -1
+            if (count >= 500) {
+                if (runTime < 120000 && start != -1) {
+                    // 1. Instantiate an AlertDialog.Builder with its constructor
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
 
-                //difference between the time of the first click and the current time
-                runTime = System.currentTimeMillis() - start;
-                //if you already won, start wibb be always -1
-                if (runTime > 10000 && start != -1) {
-                    if (count > 1) {
-                        // 1. Instantiate an AlertDialog.Builder with its constructor
-                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage(R.string.dialog_message)
+                            .setTitle(R.string.dialog_title);
 
-                        // 2. Chain together various setter methods to set the dialog characteristics
-                        builder.setMessage(R.string.dialog_message)
-                                .setTitle(R.string.dialog_title);
+                    // 3. Get the AlertDialog from create()
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    //reset the counter and save your win
+                    start = -1;
+                    try {
+                        FileOutputStream fos = new FileOutputStream(fileSave);
+                        String unlocked = "unlocked";
+                        fos.write(unlocked.getBytes());
+                        fos.close();
+                    } catch (Exception e) {
 
-                        // 3. Get the AlertDialog from create()
-                        AlertDialog dialog = builder.create();
-                        dialog.show();
-                        //reset the counter and save your win
-                        start = -1;
-                        try {
-                            FileOutputStream fos = new FileOutputStream(fileSave);
-                            String unlocked = "unlocked";
-                            fos.write(unlocked.getBytes());
-                            fos.close();
-                        } catch (Exception e) {
-
-                        }
-                        cambio.setVisibility(View.VISIBLE);
-                        count = 0;
-                        runTime = 0;
                     }
+                    cambio.setVisibility(View.VISIBLE);
+                    count = 0;
                     runTime = 0;
                 }
+                runTime = 0;
+            }
             }
         });
 
         //set the sound in relation of the switch (event handler when switch status changes)
-        cambio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                if (checked) {
-                    cambio.setText("Cameriere");
-                    suono = R.raw.cameriere;
-                } else {
-                    cambio.setText("Ding");
-                    suono = R.raw.bell;
+        cambio.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (!checked) {
+                        cambio.setText("Cameriere");
+                        suono = R.raw.cameriere;
+                        checked = !checked;
+                    } else {
+                        cambio.setText("Ding");
+                        suono = R.raw.bell;
+                        checked = !checked;
+                    }
                 }
-            }
-        });
+            });
     }
 
     //lock return button
